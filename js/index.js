22202e6374c275899eef599ca9e44297b1398869
rahp124 <punji.rahul@gmail.com>
2021-09-10 12:28:32 -0700

    adding some text

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -10,3 +10,36 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+function passValues1(){
+    localStorage.setItem("textvalue", "<strong>Project 1</strong>");  
+    return false;
+}
+function passValues2(){
+    localStorage.setItem("textvalue", "<strong>Project 2</strong>");  
+    return false;
+}
+function passValues3(){
+    localStorage.setItem("textvalue", "<strong>Project 3</strong>");  
+    return false;
+}
+function passValues4(){
+    localStorage.setItem("textvalue", "<strong>Project 4</strong>");  
+    return false;
+}
+function passValues5(){
+    localStorage.setItem("textvalue", "<strong>Project 5</strong>");  
+    return false;
+}
+function passValues6(){
+    localStorage.setItem("textvalue", "<strong>Project 6</strong>");  
+    return false;
+}
+function passValues7(){
+    localStorage.setItem("textvalue", "<strong>Project 7</strong>");  
+    return false;
+}
+window.addEventListener("load", function () {
+    var first = document.getElementById("first");
+    first.innerHTML = localStorage.getItem("textvalue");
+});
